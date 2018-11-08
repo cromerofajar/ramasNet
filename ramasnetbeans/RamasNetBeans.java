083a104190eae6afa7c5f16050290f34e644e125
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-08 09:10:27 +0100

    Rama2

--- ramasnetbeans/RamasNetBeans.java
+++ ramasnetbeans/RamasNetBeans.java
@@ -10,5 +10,9 @@ public class RamasNetBeans {
     public static void main(String[] args) {
             System.out.println("Rama principal 1");
     }
-    
+    public class RamaF{
+        public void CodigoF(){
+            System.out.println("Rama principal 2");
+        }
+    }
 }
